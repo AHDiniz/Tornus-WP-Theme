a8e791cebed36faa735ca8412682e8f1dd07b626
Alan Diniz <alanhdiniz@gmail.com>
2022-03-22 10:18:59 -0300

    Creating an activity with the creation page

--- page-criar-atividade.php
+++ page-criar-atividade.php
@@ -2,6 +2,60 @@
 
 get_header();
 
+$current_url = $_SERVER['REQUEST_URI'];
+$query_args = explode("&", $current_url);
+$query_args[0] = explode("?", $query_args[0])[1];
+$search_query = array();
+
+foreach ($query_args as $key => $string)
+{
+    $query_split = explode("=", $string);
+    $search_query[$query_split[0]] = urldecode($query_split[1]);
+}
+
+$args = array();
+
+if ($search_query['activity-name'] != null)
+{
+    $args['post_title'] = $search_query['activity-name'];
+
+    if ($search_query['activity-address'] != null && $search_query['activity-description'] && $search_query['activity-images'])
+    {
+        $content = '<div class="container mt-3 mb-3">';
+        $content .= '<div class="row"><div class="col"><h1 class="text-center es-pink">' . $args['post-title'] . '</h1></div></div>';
+        $content .= '</div>';
+
+        $content .= '<div class="container mt-3 mb-5"><div class="row"><div class="col">';
+        $content .= '<div class="row"><div class="col"><p>' . $search_query['activity-address'] . '</p></div></div>';
+        $content .= '<div class="row"><div class="col"><p>' . $search_query['activity-description'] . '</p></div></div>';
+        $content .= '</div></div></div>';
+
+        $args['post_content'] = $content;
+    }
+
+    switch ($search_query['activity-type'])
+    {
+        case 'point':
+            $args['post_type'] = 'tour_point';
+            break;
+        case 'group':
+            $args['post_type'] = 'group_activity';
+            break;
+        case 'experience':
+            $args['post_type'] = 'tour_experience';
+            break;
+        case 'event':
+            $args['post_type'] = 'event';
+            break;
+        default:
+            break;
+    }
+
+    $args['post_status'] = 'publish';
+
+    wp_insert_post($args);
+}
+
 ?>
 
 <div class="container mt-5 mb-5">
@@ -22,7 +76,7 @@ get_header();
                         <label for="activity-description">Descrição da Atividade</label>
                         <input type="text" class="form-control" placeholder="Descrição da Atividade" name="activity-description">
                         <label for="activity-images">Images da Atividade</label>
-                        <input type="image" class="form-control" accept=".jpg,.jpeg,.png" multiple>
+                        <input type="image" name="activity-images" class="form-control" accept=".jpg,.jpeg,.png" multiple>
                         <div class="row">
                             <div class="col-3 form-check">
                                 <label for="activity-type" class="form-check-label">Ponto Turístico</label>
@@ -41,6 +95,7 @@ get_header();
                                 <input type="radio" name="activity-type" value="event" class="form-check-input">
                             </div>
                         </div>
+                        <button type="submit" class="btn es-pink-bg text-white can-click rounded-pill">Criar Atividade</button>
                     </form>
                 </div>
             </div>
